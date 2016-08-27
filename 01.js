2a08b85d16651f44b26be40d514d1538f679b381
Eric Denton <eric@iamericanartist.com>
2016-08-27 12:13:17 -0500

    initial commit

---
+++ 01.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+let msg = "Yello Gourd!";
+process.stdout.write(`${msg}\n           :P\n`);
+
+// // PRACTICE/PLAYING AROUND
+// process.stdout.write(msg + " 1337"+ "\n" + "  noob\n");
+// process.stdout.write(msg + " YTMND\n");
